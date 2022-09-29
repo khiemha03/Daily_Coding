caed1c192e868eee3e3b41429a975371a593211a
Dell <minhkhiem.ha03@gmail.com>
2022-09-28 23:25:54 -0300

    Drawing pattern with turtle library

--- Turtle_1.py
+++ Turtle_1.py
@@ -1,16 +1,22 @@
-    # This is a sample Python script.
 
-from turtle import Turtle, Screen
-
+from turtle import Turtle, Screen, color
+import random
 
 def square():
+    x = random.randint(0,255)
+    y = random.randint(0,255)
+    z = random.randint(0,255)
+    pen.color(x,y,z)
     for i in range(4):
         pen.forward(100)
         pen.right(90)
 pen = Turtle()
+screen = Screen()
+screen.colormode(255)
 for _ in range(36):
     square()
     pen.left(10)
+
+
 pen.hideturtle()
-screen = Screen()
 screen.exitonclick()
