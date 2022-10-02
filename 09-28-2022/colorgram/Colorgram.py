30c988db687b652906c93e4bc18d6235b98a8f4e
Dell <minhkhiem.ha03@gmail.com>
2022-10-02 00:37:07 -0300

    Hirst painting

--- 09-28-2022/colorgram/Colorgram.py
+++ 09-28-2022/colorgram/Colorgram.py
@@ -1,3 +1,32 @@
 import colorgram
+from turtle import Turtle, Screen
+import random
 
+COLOR = []
 color_list = colorgram.extract("painting.jpg",35)
+WIDTH = 500
+LENGTH = 500
+for rgb in color_list:
+    r = rgb.rgb.r
+    g = rgb.rgb.g
+    b = rgb.rgb.b
+    COLOR.append((r,g,b))
+
+pen = Turtle()
+screen = Screen()
+screen.colormode(255)
+# screen.screensize(WIDTH, LENGTH)
+pen.penup()
+pen.goto(-410, -310)
+for i in range(8):
+    for i in range(7):
+        pen.forward(100)
+        color = random.choice(COLOR)
+        pen.dot(50, color)
+    pen.left(90)
+    pen.forward(100)
+    pen.right(90)
+    pen.back(700)
+
+
+screen.exitonclick()
